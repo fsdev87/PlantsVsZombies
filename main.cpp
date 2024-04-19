3dbb6c75e4cc541e8404e8408b8b55caa062b342
Abdullah Amin <42760671+abdullahamin231@users.noreply.github.com>
2024-04-19 20:28:03 +0500

    first commit, color should be blue

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode(200, 200), "SFML works!");
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Blue);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
