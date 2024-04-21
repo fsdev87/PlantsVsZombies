12a8768bf0734b50c7e15f1c770545bd5252e2ff
fsdev87 <mustafafaraz87@gmail.com>
2024-04-21 12:05:36 +0500

    stashing changes

--- main.cpp
+++ main.cpp
@@ -5,23 +5,11 @@ using namespace sf;
 
 int main()
 {
-<<<<<<< HEAD
-    // ABD BRANCH mus changed color to white
-    RenderWindow window(VideoMode(200, 200), "SFML works!");
-    CircleShape shape(100.f);
-    shape.setFillColor(Color::Color(255, 255, 255));
-=======
+
     // Mus Branch with blue color circle
     RenderWindow window(VideoMode(200, 200), "SFML works!");
     CircleShape shape(100.f);
     shape.setFillColor(Color::Blue);
-    // just checking to see if git status works
-    // git branch --set-upstream-to origin/main
-
-    // this is abd branch shouldn't be in main
-
-    // added to abd branch main.cpp from github.com
->>>>>>> 8fbb453d9a7e64302afdae72fbb6a1049dc048b9
 
     while (window.isOpen())
     {
