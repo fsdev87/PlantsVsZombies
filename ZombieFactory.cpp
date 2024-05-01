70aa9a9251f6a9e61b932b027bbb03440740c5c5
Mustafa Faraz <mustafafaraz87@gmail.com>
2024-05-01 12:05:08 +0500

    some this-> issues fix in zombiefactory

--- ZombieFactory.cpp
+++ ZombieFactory.cpp
@@ -5,57 +5,57 @@ using namespace sf;
 
 
 ZombieFactory::ZombieFactory(TextureManager* tm) {
-	zombies = new Zombie * [maxZombies] { nullptr };
+	this->zombies = new Zombie * [this->maxZombies] { nullptr };
 	this->TMptr = tm;
-	deadIndex = -1;
-	zombiesArrayIndex = 0;
-	spawnClock.restart();
+	this->deadIndex = -1;
+	this->zombiesArrayIndex = 0;
+	this->spawnClock.restart();
 }
 
 void ZombieFactory::getZombieDeadIndex() {
-	for (int i = 0; i < zombiesArrayIndex; i++) {
-		if (zombies[i]->getExist() == false) {
-			deadIndex = i;
+	for (int i = 0; i < this->zombiesArrayIndex; i++) {
+		if (this->zombies[i]->getExist() == false) {
+			this->deadIndex = i;
 			cout << "Found dead index";
 			return;
 		}
 	}
-	deadIndex = -1;
+	this->deadIndex = -1;
 }
 
 void ZombieFactory::spawnZombie() {
-	if (spawnClock.getElapsedTime().asSeconds() < 5) return;
+	if (this->spawnClock.getElapsedTime().asSeconds() < 5) return;
 
 	getZombieDeadIndex();
-	pos[1] = rand() % 5;
-	if (deadIndex != -1) {
+	this->pos[1] = rand() % 5;
+	if (this->deadIndex != -1) {
 		cout << "dead index is not -1\n";
-		delete[] zombies[deadIndex];
-		zombies[deadIndex] = new NormalZombie(TMptr->getTexture("spritesheet-nZombWalk"), 22, pos, TMptr);
+		delete[] this->zombies[this->deadIndex];
+		this->zombies[this->deadIndex] = new NormalZombie(this->TMptr->getTexture("spritesheet-nZombWalk"), 22, this->pos, this->TMptr);
 	}
 	else {
-		if (zombiesArrayIndex < maxZombies) {
-			zombies[zombiesArrayIndex] = new NormalZombie(TMptr->getTexture("spritesheet-nZombWalk"), 22, pos, TMptr);
-			zombiesArrayIndex++;
+		if (this->zombiesArrayIndex < this->maxZombies) {
+			this->zombies[this->zombiesArrayIndex] = new NormalZombie(this->TMptr->getTexture("spritesheet-nZombWalk"), 22, this->pos, this->TMptr);
+			this->zombiesArrayIndex++;
 		}
 		else cout << "Max zombies reached in spawnZombie function" << endl;
 	}
-	spawnClock.restart();
+	this->spawnClock.restart();
 }
 
 Zombie** ZombieFactory::getZombies() { return this->zombies; }
 int& ZombieFactory::getZombiesArrayIndex() { return this->zombiesArrayIndex; }
 
 void ZombieFactory::updateEverything(Plant** plants, int plantsArrayIndex) {
-	for (int i = 0; i < zombiesArrayIndex; i++) {
-		zombies[i]->animate();
-		zombies[i]->move(plants, plantsArrayIndex);
+	for (int i = 0; i < this->zombiesArrayIndex; i++) {
+		this->zombies[i]->animate();
+		this->zombies[i]->move(plants, plantsArrayIndex);
 	}
 	spawnZombie();
 }
 
 void ZombieFactory::draw(RenderWindow& window) {
-	for (int i = 0; i < zombiesArrayIndex; i++) {
-		zombies[i]->draw(window);
+	for (int i = 0; i < this->zombiesArrayIndex; i++) {
+		this->zombies[i]->draw(window);
 	}
 }
